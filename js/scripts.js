9a133ca29f96137502c9c39d7e7affde20cfae54
Collin Person and Stephanie Mayer <collin@>
2017-07-19 15:28:39 -0700

    added stuff

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,23 @@
 function LetsPlay (){
   this.gameDice = new Dice();
   this.player1 = new Player1();
-
 function Dice(dice) {
   this.dice = 6;
 }
 Dice.prototype.newDice = function (dice) {
   return  Math.floor((Math.random() * this.dice) + 1)
 }
-Player1.prototype.newRolled = function (dice)  {
-  return this.turn + this.total
-}
+// Player1.prototype.newRolled = function ()  {
+//    this.total += this.turn;
+//    return this.turn = 0;
+// }
+debugger;
 Player1.prototype.score = function (thisRoll) {
     if (thisRoll === 1) {
       alert("turn over")
     return this.turn = 0;
     }else {
-      return this.turn += thisRoll;
+      this.turn += thisRoll;
     }
   }
 function Player1() {
@@ -26,25 +27,22 @@ function Player1() {
   this.turn = 0;
   this.total = 0;
   }
-// }
-
-
+}
 $(function(){
   var game = new LetsPlay();
   $(".btn").click(function(){
     var dice = game.gameDice.newDice();
-    game.player1.score(dice);
-    //var roll = game.player1.turn += dice;
-     //game.player1.score();
-    console.log(dice);
-    console.log(game.player1.score());
-  //  console.log(roll);
-
+    //var score = game.gameDice.newDice()
+    console.log(dice)
     $(".numberrolled").text(dice);
-    $(".number1").text(game.player1.score());
+    $(".number1").text(game.player1.total);
  });
+ // $(".btn1").click(function(){
+ //   var dice1 = game.gameDice.newDice();
+ //   game.player
+ // });
 });
-// };
+
 
 // function myFunction() {
 //
