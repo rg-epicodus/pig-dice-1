cf526f375aa4c8cb68582ee44f74b5c64550ac00
Collin Person and Stephanie Mayer <stephanie@>
2017-07-19 16:25:05 -0700

    add style

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 //Business Logic
 function LetsPlay (){
   this.gameDice = new Dice();
-  this.player1 = new Player1();
+  this.player1 = new Player();
+  this.player2= new Player();
 function Dice(dice) {
   this.dice = 6;
 }
 Dice.prototype.newDice = function () {
   return  Math.floor((Math.random() * this.dice) + 1)
 }
-Player1.prototype.newRolled = function ()  {
+Player.prototype.newRolled = function ()  {
    this.total += this.turn;
    return this.turn = 0;
 }
-Player1.prototype.score = function (thisRoll) {
+Player.prototype.score = function (thisRoll) {
     if (thisRoll === 1) {
       alert("turn over");
     return this.turn = 0;
@@ -21,8 +22,8 @@ Player1.prototype.score = function (thisRoll) {
       this.total += thisRoll;
     }
   }
-function Player1() {
-  this.player = 'Player 1';
+function Player() {
+  this.player = 'Player:';
   this.turn = 0;
   this.total = 0;
   }
@@ -31,24 +32,27 @@ $(function(){
   var game = new LetsPlay();
   $(".btn").click(function(){
     var dice = game.gameDice.newDice();
-    //var score = game.gameDice.newDice()
-    console.log(dice)
     game.player1.score(dice);
     $(".numberrolled").text(dice);
     $(".number1").text(game.player1.turn);
-
-    // if $("stop1").clicked {
-    //   add total of accumlated score 
-
-
-    }
  });
- // $(".btn1").click(function(){
- //   var dice1 = game.gameDice.newDice();
- //   game.player
- // });
+  $(".stop").click(function() {
+    $(".number1").text(game.player1.total);
+  });
+  $(".btn1").click(function(){
+    var dice = game.gameDice.newDice();
+    game.player2.score(dice);
+    $(".numberrolled").text(dice);
+    $(".number2").text(game.player2.turn);
+  $(".stop1").click(function() {
+    $(".number2").text(game.player2.total);
+  });
+  });
 });
-
+// $(".btn1").click(function(){
+//   var dice1 = game.gameDice.newDice();
+//   game.player
+// });
 
 // function myFunction() {
 //
